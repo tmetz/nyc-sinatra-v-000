891f2c772c2af48c9f1ed3499ad24c36861b7a85
Tammy Metz <tammy@tammymetz.com>
2018-12-25 17:13:10 +0000

    Automatically backed up by Learn

--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "Title" do
-  before do 
-    @figure = Figure.create(:name => "Fiorello LaGuardia") 
+  before do
+    @figure = Figure.create(:name => "Fiorello LaGuardia")
     @mayor = Title.create(:name => "Mayor")
   end
 
@@ -13,11 +13,11 @@ describe "Title" do
   end
 
 
-  it "has a name" do 
+  it "has a name" do
     expect(@mayor.name).to eq("Mayor")
   end
 
-  it "has many figures" do 
+  it "has many figures" do
     @mayor.figures << @figure
     expect(@mayor.figures).to include(@figure)
   end
